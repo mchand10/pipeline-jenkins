4536d30de256697af59e456e539c3d3c0647f159
mchand10 <mitcmohan2200@gmail.com>
2019-11-11 01:04:15 +0530

    dir updated

--- Test.sh
+++ Test.sh
@@ -4,4 +4,4 @@ echo "******** Testing Maven ********"
 
 WORKSPACE=/var/jenkins_home/workspace
 
-docker run --rm -i -v $WORKSPACE:/app -v ./downloads:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -i -v $WORKSPACE:/app -v downloads:/root/.m2/ -w /app maven:3-alpine "$@"
