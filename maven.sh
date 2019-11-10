acd3e86c4ab5a67bdfbf090bd495d67ec2573bb0
mchand10 <mitcmohan2200@gmail.com>
2019-11-11 01:56:26 +0530

    workspaces

--- maven.sh
+++ maven.sh
@@ -4,4 +4,4 @@ echo "**********STARTING MAVEN BUILD*********"
 
 WORKSPACE="/var/jenkins_home/workspace/pipeline-docker-mvn"
 
-docker run --rm -i -v $WORKSPACE/app -v downloads:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -i -v $WORKSPACE:/app -v downloads:/root/.m2/ -w /app maven:3-alpine "$@"
