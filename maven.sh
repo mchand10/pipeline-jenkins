e341681929be6376eab647cb5b1f534708ac934a
mchand10 <mitcmohan2200@gmail.com>
2019-11-11 00:57:31 +0530

    Removed docker TTY -t

--- maven.sh
+++ maven.sh
@@ -4,4 +4,4 @@ echo "**********STARTING MAVEN BUILD*********"
 
 WORKSPACE="/var/jenkins_home/workspace"
 
-docker run --rm -it -v $PWD:/app -v $WORKSPACE/downloads:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -i -v $PWD:/app -v $WORKSPACE/downloads:/root/.m2/ -w /app maven:3-alpine "$@"
